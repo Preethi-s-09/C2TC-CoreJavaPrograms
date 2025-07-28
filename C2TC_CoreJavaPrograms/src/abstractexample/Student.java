b50cf32efff606bb059dfe2ea4e2267c7a6509d9
Preethi-SS <preethicse2026@gmail.com>
2025-07-28 20:55:27 +0530

    java programs

---
+++ C2TC_CoreJavaPrograms/src/abstractexample/Student.java
@@ -0,0 +1,10 @@
+package abstractexample;
+
+public class Student extends Coder {
+	@Override
+	public void rules() {
+		System.out.println("Morning wakkeup at "+wakeup);
+		System.out.println("And do  the program");
+	}
+
+}
